d200737c4aa7274ad6f31146dbec819761b7037b
home2mars <home2mars@gmail.com>
2018-05-16 21:04:52 -0400

    Implementing better if/else statement to fulfill the project rubric

--- app/src/main/java/com/example/android/meteorologyquiz/MainActivity.java
+++ app/src/main/java/com/example/android/meteorologyquiz/MainActivity.java
@@ -12,15 +12,13 @@ import android.widget.RadioButton;
 import android.widget.Toast;
 
 
-
 public class MainActivity extends AppCompatActivity {
 
-    int score = 0;
+    int score;
 
     RadioButton answerOne, answerTwo, answerThree, answerFour;
     CheckBox answerFiveA, answerFiveB, answerFiveC, answerFiveD;
     EditText answerSix;
-
 
 
     @Override
@@ -44,49 +42,69 @@ public class MainActivity extends AppCompatActivity {
         answerSix = findViewById(R.id.answer6);
     }
 
-    /*  Submits answers, calculates score */
+    /**
+     * Submits answers, calculates score
+     */
     public void scoreQuiz(View view) {
         int finalScore = checkAnswers();
 
-        if (finalScore < 0) {
-            Toast.makeText(this, "Please answer all questions", Toast.LENGTH_SHORT).show();
+        if (finalScore < 3) {
+            Toast.makeText(this, getString(R.string.tryAgainMessage), Toast.LENGTH_SHORT).show();
             score = 0;
-            return;
+        } else {
+            Button submitButton = findViewById(R.id.submitButton);
+            submitButton.setEnabled(false);
+            Toast.makeText(this, String.format(getString(R.string.scoreMessage), finalScore), Toast.LENGTH_SHORT).show();
+            score = 0;
         }
-
-        Button submitButton = findViewById(R.id.submitButton);
-        //submitButton.setEnabled(false);
-        Toast.makeText(this, String.format(getString(R.string.scoreMessage), finalScore), Toast.LENGTH_SHORT).show();
-        score = 0;
     }
 
-    /* Resets the score to zero */
+    /**
+     * Resets the score to zero
+     */
     public void resetQuiz(View view) {
         Intent intent = getIntent();
         finish();
         startActivity(intent);
     }
 
-    //Check Answers
+    /**
+     *    Checking the Answers
+     *
+     *    @return int score
+     *
+     */
     private int checkAnswers() {
         // Question 1 RadioButton - Correct Answer is B (Climate)
-        if (answerOne.isChecked()) {score++;}
+        if (answerOne.isChecked()) {
+            score++;
+        }
 
         // Question 2 RadioButton - Correct Answer is D (Energy use)
-        if (answerTwo.isChecked()) {score++;}
+        if (answerTwo.isChecked()) {
+            score++;
+        }
 
         // Question 3:  RadioButton, Correct Answer B (Weather)
-        if (answerThree.isChecked()) {score++;}
+        if (answerThree.isChecked()) {
+            score++;
+        }
 
         // Question 4:  RadioButton, Correct Answer D (All of these are factors that influence temperature)
-        if (answerFour.isChecked()) {score++;}
+        if (answerFour.isChecked()) {
+            score++;
+        }
 
         // Question 5: Checkboxes, Correct Answer(s): all 3
-        if (answerFiveA.isChecked() && answerFiveB.isChecked() && answerFiveC.isChecked() && !answerFiveD.isChecked()) {score++;}
+        if (answerFiveA.isChecked() && answerFiveB.isChecked() && answerFiveC.isChecked() && !answerFiveD.isChecked()) {
+            score++;
+        }
 
         // Question 6: Edit Text, Correct Answer: Troposhere
         String strAnswer6 = answerSix.getText().toString();
-        if (strAnswer6.equalsIgnoreCase("troposphere")) {score++;}
+        if (strAnswer6.equalsIgnoreCase("troposphere")) {
+            score++;
+        }
 
         return score;
     }
